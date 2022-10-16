ec52d268f69ab7316f3cd0f7e88f4018aa8883a8
Alan LI <54613614+Kazee0@users.noreply.github.com>
2022-10-16 13:16:16 -0700

    Create test for reading files

--- tests/test_project1.py
+++ tests/test_project1.py
@@ -1,3 +1,12 @@
 import unittest
-
-
+from pathlib import Path
+from IO_file import read_instructions_from_file
+class MyTestCase(unittest.TestCase):
+    def test_file(self):
+        """Test the opening and reading of the given files."""
+        path = Path('test.txt')
+        self.assertEqual(read_instructions_from_file(path), ['PROPAGATE 1 2 750','PROPAGATE 2 3 1250'])
+        path = Path('nothing.txt')
+        self.assertEqual(read_instructions_from_file(path),None)
+if __name__ == '__main__':
+    unittest.main()
