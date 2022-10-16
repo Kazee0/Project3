3035027adc1e31a2f77c3a60174a74488faac298
Alan LI <54613614+Kazee0@users.noreply.github.com>
2022-10-16 13:19:57 -0700

    Write three classes that help the running of program

--- project1.py
+++ project1.py
@@ -1,6 +1,39 @@
 from pathlib import Path
 
 
+class situation:
+    def __init__(self, types, msg):
+        self.type = types
+        self.msg = msg
+
+class DEVICE:
+    def __init__(self, ID):
+        self._ID = ID
+        self.situation = []
+    def propagate(self,from_ID, to_ID, time):
+        pass
+    def add_situation(self, sit):
+        self.situation.append(sit)
+    def show_id(self):
+        return self._ID
+
+    def handle_cancel(self, msg):
+        flag = False
+        for i in range(len(self.situation)):
+            if self.situation[i].msg == msg:
+                self.situation.pop(i)
+                flag = True
+        if not flag:
+            si = situation('CANCEL', msg)
+            self.add_situation(si)
+
+class PROPAGATE:
+    def __init__(self, time, types, from_ID, to_ID):
+        self.type = types
+        self.time = time
+        self.from_ID = from_ID
+        self.to_ID = to_ID
+
 def _read_input_file_path() -> Path:
     """Reads the input file path from the standard input"""
     return Path(input())
