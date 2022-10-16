caa9e3a28ac1b6095404031a31e56b59e8d53255
Alan LI <54613614+Kazee0@users.noreply.github.com>
2022-10-16 13:17:53 -0700

    Create test for managing instructions in same time

--- tests/test_project1.py
+++ tests/test_project1.py
@@ -1,6 +1,7 @@
 import unittest
 from pathlib import Path
 from IO_file import read_instructions_from_file
+from conflicts import find_conflict
 class MyTestCase(unittest.TestCase):
     def test_file(self):
         """Test the opening and reading of the given files."""
@@ -8,5 +9,17 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(read_instructions_from_file(path), ['PROPAGATE 1 2 750','PROPAGATE 2 3 1250'])
         path = Path('nothing.txt')
         self.assertEqual(read_instructions_from_file(path),None)
+    def test_the_conflicts(self):
+        """Test the function of finding conflicts instructions."""
+        text_input = [
+            'ALERT 2 Trouble 100',
+            'ALERT 3 Trouble 100',
+            'CANCEL 1 OH 10'
+        ]
+        text_output = {
+            '100': ['ALERT 2 Trouble 100','ALERT 3 Trouble 100'],
+            '10':['CANCEL 1 OH 10']
+        }
+        self.assertEqual(find_conflict(text_input), text_output)
 if __name__ == '__main__':
     unittest.main()
